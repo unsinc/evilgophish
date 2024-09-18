f01b6883540924e8bf1ab58f65d0ee79a0024982
Nikolay Kolev <toteb@abv.bg>
2024-09-18 10:53:33 +0300

    default phish fixed

--- gophish/controllers/api/util.go
+++ gophish/controllers/api/util.go
@@ -34,14 +34,14 @@ func (as *Server) SendTestEmail(w http.ResponseWriter, r *http.Request) {
 	// If a Template is not specified use a default
 	if s.Template.Name == "" {
 		//default message body
-		text := "It works!\n\nThis is an email letting you know that your gophish\nconfiguration was successful.\n" +
-			"Here are the details:\n\nWho you sent from: {{.From}}\n\nWho you sent to: \n" +
+		text := "It works! Configuration was successful.\n" +
+			"Who you sent from: {{.From}}\n\nWho you sent to: \n" +
 			"{{if .FirstName}} First Name: {{.FirstName}}\n{{end}}" +
 			"{{if .LastName}} Last Name: {{.LastName}}\n{{end}}" +
 			"{{if .Position}} Position: {{.Position}}\n{{end}}" +
-			"\nNow go send some phish!"
+			"\nNow go send some mails!"
 		t := models.Template{
-			Subject: "Default Email from Gophish",
+			Subject: "Default Email",
 			Text:    text,
 		}
 		s.Template = t
